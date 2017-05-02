5f4a65c532e33554cb3d6e0f5790e709981f206e
abdelrahmansamir <abdelrahmanhalawa@gmail.com>
2017-05-03 00:20:02 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -2,13 +2,16 @@
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileURL, "household_power_consumption.zip")
 unzip("household_power_consumption.zip")
-eData <- read.csv("household_power_consumption.txt",sep = ";")
-## step 2 -> cleans data
-dataReq <- subset(eData, eData$Date == "1/2/2007" | eData$Date == "2/2/2007")
-dataReq$newDate <- with(dataReq, as.POSIXct(paste(Date, Time, sep = " "), format="%d/%m/%Y %H:%M:%S"))
-apwr <- as.numeric(dataReq$Global_active_power)
-## creates Plot 1
-png("plot1.png", width = 480, height = 480)
-hist(apwr, freq = TRUE, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
-dev.off()
+ptable <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
 
+## subset out the two days we are looking for
+pdata <-  subset(ptable, Date == "1/2/2007" | Date == "2/2/2007")
+
+##open .png file
+png("plot1.png", width = 480, height = 480)
+
+## draw red histogram of global active power with an x label
+hist(pdata$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+
+##close png file
+dev.off()
