bf4924fafb939dace8ed755cd7246d8d61ae92ac
abdelrahmansamir <abdelrahmanhalawa@gmail.com>
2017-05-02 23:59:11 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,12 +1,14 @@
+## step 1 -> download data
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileURL, "./household_power_consumption.txt", method = "curl")
+download.file(fileURL, "household_power_consumption.zip")
 unzip("household_power_consumption.zip")
-electricData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
-## cleans data
-desiredData <- subset(electricData, electricData$Date == "1/2/2007" | electricData$Date == "2/2/2007")
-desiredData$newDate <- with(desiredData, as.POSIXct(paste(Date, Time, sep = " "), format="%d/%m/%Y %H:%M:%S"))
-pwr <- as.numeric(desiredData$Global_active_power)
+eData <- read.csv("household_power_consumption.txt",sep = ";")
+## step 2 -> cleans data
+dataReq <- subset(eData, eData$Date == "1/2/2007" | eData$Date == "2/2/2007")
+dataReq$newDate <- with(dataReq, as.POSIXct(paste(Date, Time, sep = " "), format="%d/%m/%Y %H:%M:%S"))
+apwr <- as.numeric(dataReq$Global_active_power)
 ## creates Plot 1
 png("plot1.png")
-hist(pwr, freq = TRUE, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+hist(apwr, freq = TRUE, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
 dev.off()
+
